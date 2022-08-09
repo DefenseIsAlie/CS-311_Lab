d402a570d739ffb334de25623149b6b3c94cb1b2
DefenseIsAlie <200010021@iitdh.ac.in>
2022-08-09 18:13:12 +0530

    improved run.py

--- Lab_0/run.py
+++ Lab_0/run.py
@@ -2,15 +2,42 @@ import os
 import python_scripts.compile_and_run
 import python_scripts.plot
 
-dirs = os.listdir()
-if "Lab_0" in dirs:
-    os.chdir(os.getcwd()+"/Lab_0")
-folder_pth = os.getcwd()
+# Check if numpy and python are installed
+try:
+    import numpy
+    import matplotlib
+except:
+    print("Numpy and Matplotlib must be installed")
+    print("try --> $ pip install -r requirements.txt")
 
-python_scripts.compile_and_run.compile_java_file(folder_pth)
+# Dangeorus , change it
+def Search_dirs_for_file():
+    dirs = os.listdir()
+    
+    for directory in dirs:
+        pth = os.getcwd() + "/" + directory
+        try:
+            if "Main.java" in os.listdir(pth) and directory == "Lab_0":
+                os.chdir(pth)
+                collect_data_and_plot()
+        except:
+            pass 
 
-python_scripts.compile_and_run.run_java_file(folder_pth)
+def collect_data_and_plot():
+    folder_pth = os.getcwd()
 
-python_scripts.plot.collect_data(folder_pth)
+    # Compiles the java file
+    python_scripts.compile_and_run.compile_java_file(folder_pth)
 
-python_scripts.plot.plot_data()
+    # Test runs the java file
+    python_scripts.compile_and_run.run_java_file(folder_pth)
+
+    # Runs the program for 10 times and plots the data
+    python_scripts.plot.collect_data(folder_pth)
+    python_scripts.plot.plot_data()
+
+
+if "Main.java" not in os.listdir():
+    Search_dirs_for_file()
+else: 
+    collect_data_and_plot()
