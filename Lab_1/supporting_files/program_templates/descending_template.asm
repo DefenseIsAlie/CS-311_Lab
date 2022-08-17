37ff7a80b7474e4ca37b642c1ed659d78da93e0c
DefenseIsAlie <200010021@iitdh.ac.in>
2022-08-17 18:41:24 +0530

    sorting algo

--- Lab_1/supporting_files/program_templates/descending_template.asm
+++ Lab_1/supporting_files/program_templates/descending_template.asm
@@ -11,6 +11,30 @@ a:
 n:
 	8
 	.text
-// your code here
-// you may change the numbers in the array, and the size of the array; but allow the name of the array to remain as 'a', and size as 'n'
-// remove these comments!
+main:
+	load %x0, $n, %x30
+	subi %x30, 1, %x30
+	subi %30, 1, %x29
+	sub %x0, 1, %x15
+	sub %x0, 1, %x3
+	jmp mainloop
+mainloop:
+	beq %x30, %x15, exitsuccess
+	addi %x15, 1, %x15
+	jmp steploop
+steploop:
+	beq %x29, %x3, mainloop
+	addi %x3, 1, %x3
+	addi %x3, 1, %x4
+	load %x3, $a, %x10
+	addi %x3, $a, %x7
+	load %x4, $a, %x11
+	addi %x4, $a, %x8
+	blt  %x10, %x11, swap
+	jmp steploop
+swap:
+	store %x3, 10, %x3
+	store %x4, 10, %x4
+	jmp steploop
+exitsuccess:
+	end
